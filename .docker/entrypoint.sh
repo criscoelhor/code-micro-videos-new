ef4296108220fb8d14f896a6d6770465cc42164b
criscoelhor <coelhocr@gmail.com>
2021-02-06 23:36:33 -0300

    update entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+chmod -R 777 /var/www/backend/storage && chmod -R 777 /var/www/backend/bootstrap/cache
+
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 ### FRONT-END
 npm config set cache /var/www/.npm-cache --global
 cd /var/www/frontend && npm install && cd ..
 
 ### BACK-END
-cd backend
-if [ ! -f ".env" ]; then
-  cp .env.example .env
-fi
+cd backend 
 if [ ! -f ".env.testing" ]; then
-  cp .env.testing.example .env.testing
+    cp .env.testing.exemple .env.testing
 fi
-#chown -R www-data:www-data .
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
 composer install
 php artisan key:generate
 php artisan migrate
